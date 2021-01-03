74908886f73814aa00ded98ea89ceb5854023ec5
MilhamAt <M477ashiddiq@gmail.com>
2021-01-03 16:33:20 +0700

    Initial Commit

---
+++ SimpleUnitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleUnitTest
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 03/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
