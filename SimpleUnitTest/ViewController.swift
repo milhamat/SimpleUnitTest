10908d16fcd57c47602d70f79dfcbd937bc46cda
MilhamAt <M477ashiddiq@gmail.com>
2021-01-03 16:53:52 +0700

    first add view

--- SimpleUnitTest/ViewController.swift
+++ SimpleUnitTest/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let boxView = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        boxView.backgroundColor = .gray
+        setupView()
+    }
+    
+    func setupView(){
+        
+        self.view.addSubview(self.boxView)
+        
+        let constVal: Int = 100
+        
+        boxView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            boxView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            boxView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            boxView.heightAnchor.constraint(equalToConstant: CGFloat(constVal)),
+            boxView.widthAnchor.constraint(equalToConstant: CGFloat(constVal))
+        ])
     }
 
 
